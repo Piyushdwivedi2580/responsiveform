3d9a764c0195cff18d71a029e21a5d2dd44c5fb1
decon <piyushdwivedi2580.com>
2021-07-14 10:56:48 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'form2';
+  
+  // getValue(value:any){
+  //   console.log(value);
+  //   }
+  
+  // ngOnInit(): void{
+  //   let email = document.getElementById('email');
+  //   console.log(email);
+  // }
 }
+
